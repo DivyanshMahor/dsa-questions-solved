240bf5db3346ce450013e656d925950218cb5cfd
Divyansh <160347470+DivyanshMahor@users.noreply.github.com>
2026-07-24 22:49:14 +0530

    Time: 0 ms (100%), Space: 43.7 MB (25.31%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,33 +1,42 @@
 class Solution {
     public void sortColors(int[] nums) {
 
-        int low = 0;
+        int  st = 0;
         int mid = 0;
-        int high = nums.length - 1;
+        int end = nums.length-1;
+        // int n = 
 
-        while (mid <= high) {
+        while( mid <= end ){ 
 
-            if (nums[mid] == 0) {
-                // 0 ko left side bhejo
-                int temp = nums[low];
-                nums[low] = nums[mid];
-                nums[mid] = temp;
+            if( nums[mid] == 0 ){
 
-                low++;
+                swap(nums, st , mid );
+                st++;
                 mid++;
-
-            } else if (nums[mid] == 1) {
-                // 1 already middle me sahi hai
-                mid++;
-
-            } else {
-                // 2 ko right side bhejo
-                int temp = nums[mid];
-                nums[mid] = nums[high];
-                nums[high] = temp;
-
-                high--;
+                
             }
-        }
+
+            else if( nums[mid] == 2 ){
+
+                swap(nums, end , mid );
+                end--;
+            }
+
+
+            else{ // arr[m] == 1;
+            
+            mid++;
+
+            }
+
+
+         }
+        
+    }
+
+    public void swap( int[] arr, int po1, int po2){
+        int temp = arr[po1];
+        arr[po1] = arr[po2];
+       arr[po2] = temp;
     }
 }
