ac8a441959e4df03b91d5b7be0b30966d66a6b27
Divyansh <160347470+DivyanshMahor@users.noreply.github.com>
2026-08-08 20:01:06 +0530

    Time: 50 ms (87.68%), Space: 65.3 MB (97.36%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -1,34 +1,31 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
     public int totalFruit(int[] fruits) {
-
-        Map<Integer, Integer> freq = new HashMap<>();
+        Map<Integer, Integer> frequency = new HashMap<>();
 
         int left = 0;
-        int maxLen = 0;
+        int longest = 0;
 
         for (int right = 0; right < fruits.length; right++) {
+            frequency.merge(fruits[right], 1, Integer::sum);
 
-            // Add current fruit
-            freq.put(fruits[right], freq.getOrDefault(fruits[right], 0) + 1);
+            while (frequency.size() > 2) {
+                int fruit = fruits[left];
 
-            // Shrink window until only 2 fruit types remain
-            while (freq.size() > 2) {
+                frequency.merge(fruit, -1, Integer::sum);
 
-                int leftFruit = fruits[left];
-
-                freq.put(leftFruit, freq.get(leftFruit) - 1);
-
-                if (freq.get(leftFruit) == 0) {
-                    freq.remove(leftFruit);
+                if (frequency.get(fruit) == 0) {
+                    frequency.remove(fruit);
                 }
 
                 left++;
             }
 
-            // Update answer
-            maxLen = Math.max(maxLen, right - left + 1);
+            longest = Math.max(longest, right - left + 1);
         }
 
-        return maxLen;
+        return longest;
     }
 }
